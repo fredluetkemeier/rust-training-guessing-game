8a757a9bd0d294cf50c6c17acbc47ba7619018e3
fredluetkemeier <fredluetkemeier@gmail.com>
2022-01-06 00:23:33 -0600

    Finished fibonacci sequence

--- fibonacci-sequence/src/main.rs
+++ fibonacci-sequence/src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let index: u64 = args[1].parse().expect("I need a positive integer!");
+
+    let mut last = 0;
+    let mut current = 1;
+
+    println!("{}\n{}", last, current);
+
+    for _ in 1..index {
+        let next = next_in_sequence(last, current);
+
+        println!("{}", next);
+
+        last = current;
+        current = next;
+    }
+}
+
+fn next_in_sequence(last: u128, current: u128) -> u128 {
+    last + current
 }
